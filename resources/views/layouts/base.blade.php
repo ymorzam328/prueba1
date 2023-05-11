4bd5d9c35148766158b39db3a5c22ababbfff6c3
ymorzam328 <ymorzam328@g.educaand.es>
2023-05-11 10:14:21 +0200

    Cuarto commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,11 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <title>Document</title>
+    <!-- Librerias JS -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Estilos Bootstrap -->
+    <link href="{{ asset('css/app.css') }}" rel="styleheet">
 </head>
 <body>
-    <h1>Bienvenido al sistema</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+        <a class="navbar-brand"
+            href="#">Navbar
+        </a>
+
+        <button class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse">
+    
+    
+
+
+    <!-- <h1>Bienvenido al sistema</h1>
     <h3>Hola usuario ha ingresado correctamente</h3>
     <div class="container">
         <div class="col-md-9 justify-content">
@@ -16,7 +34,7 @@
             debajo se colocará todo el contenido dinámico.
             @yield('contenido')
         </div>
-    </div>
+    </div> -->
 
 </body>
 </html>
